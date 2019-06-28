240fa9e976dd02a7804c31a7dc64b51342ca9822
Bilalahmad4554 <bilalahmad4554@gmail.com>
2019-06-28 11:47:42 +0500

    Code Refactoring.

--- app/src/main/java/com/example/newsreader/MainActivity.java
+++ app/src/main/java/com/example/newsreader/MainActivity.java
@@ -2,38 +2,29 @@ package com.example.newsreader;
 
 import android.content.Intent;
 import android.os.Bundle;
-
 import com.example.newsreader.common.HelperVariables;
 import com.example.newsreader.fragments.DailyNewsFeedFragment;
 import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
 import com.google.android.material.snackbar.Snackbar;
-
 import android.util.Log;
 import android.view.View;
-
 import androidx.annotation.NonNull;
 import androidx.core.view.GravityCompat;
 import androidx.appcompat.app.ActionBarDrawerToggle;
-
 import android.view.MenuItem;
-
 import com.google.android.material.navigation.NavigationView;
 import com.google.firebase.iid.FirebaseInstanceId;
 import com.google.firebase.iid.InstanceIdResult;
 import com.google.firebase.messaging.FirebaseMessaging;
 import com.wang.avi.AVLoadingIndicatorView;
-
 import androidx.drawerlayout.widget.DrawerLayout;
-
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 import androidx.fragment.app.Fragment;
 import androidx.fragment.app.FragmentTransaction;
-
 import android.view.Menu;
-
 import butterknife.BindView;
 import butterknife.ButterKnife;
 
